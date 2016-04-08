45423f24c0f7536bbc2e58a742412844c6c07dda
peterlin424 <peterlin424@gmail.com>
2016-04-08 18:33:15 +0800

    first

---
+++ app/src/main/java/project/peter/com/vuforiarajawali3d/MainActivity.java
@@ -0,0 +1,25 @@
+package project.peter.com.vuforiarajawali3d;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ARActivity.class));
+            }
+        });
+
+    }
+}
